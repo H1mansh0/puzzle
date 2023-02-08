4377083fa88f23f0cbe905baf0322543ed9b89a6
maijamana <97440512+maijamana@users.noreply.github.com>
2023-02-08 09:37:46 +0200

    Create blockchecker

--- puzzle.py
+++ puzzle.py
@@ -5,7 +5,23 @@ def transformer(board):
     pass
 
 def blockchecker(board):
-    pass
+    '''
+    This function returns True if blocks is True.
+    >>> blockchecker(["**** ****","***  ****","**  3****","* 3 1****","     9 5 "," 6  60  *","3  98  **","  8  2***","  4  ****"])
+    ['****  3   4  ****', '***  6  8  2***', '** 3   98  **', '*     60  *', '  31 9 5 ', '*******', '*****', '***', '*']
+    >>> blockchecker('Text!')
+    False
+    '''
+    if not isinstance(board,list):
+        return False
+    columns = transformer(board)
+    rows = board
+
+    new_rows = []
+    for i in range(len(columns)):
+        new_row = columns[i][:-i-1]+rows[-i-1][i:]
+        new_rows += [new_row]
+    return new_rows
 
 def validate_board(board) -> bool:
     pass
