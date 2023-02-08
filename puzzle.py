1e605a2c85c0315b29650056b3bf24583b5eefaa
PavloKukurik <116553132+PavloKukurik@users.noreply.github.com>
2023-02-08 09:44:52 +0200

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -17,19 +17,8 @@ def gorizontal_checking(board: list[str]) -> bool:
 
 
 def transformer(board: list) -> list:
-    '''
-    This function returns list of shifted rows to columns.
-    >>> transformer(["**** ****","***  ****","  3****","* 3 1****","     9 5 "," 6  60  *","3  98  ","  8  2***","  4  ****"])
-    ['****  3  ', '***  6   ', ' 3   84', '*     9  ', '  31 68  ', '****90 2*', '****   ', '****5 ***', '**** ****']
-    >>> transformer('Hello!')
-    False
-    '''
-    if not isinstance(board,list):
-        return False
-    columns = []
-    for i in range(len(board[0])):
-        columns.append(''.join([j[i] for j in board]))
-    return columns
+    pass
+
 
 def blockchecker(board):
     pass
